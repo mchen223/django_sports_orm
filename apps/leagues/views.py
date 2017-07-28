b70e6fc7dd4e1af046a30d67e68096e71ab3227b
Melissa Chen <chenmelissal@gmail.com>
2017-07-28 05:31:21 -0700

    Added ORM II queries. Still need to work on #14, but everything is else there and functional.

--- apps/leagues/views.py
+++ apps/leagues/views.py
@@ -4,6 +4,7 @@ from . import team_maker
 from django.http import HttpResponse
 from django.template import Context
 from django.contrib import messages
+from django.db.models import Count
 
 def index(request):
 	try:
@@ -94,4 +95,64 @@ def filter(request):
 	elif number == 16:
 		context = {
 			"players": Player.objects.filter(first_name='Alexander')|Player.objects.filter(first_name='Wyatt')}
+	elif number == 17:
+		context = {
+			"teams": Team.objects.filter(league__name='Atlantic Soccer Conference')}
+	elif number == 18:
+		context = {
+			"players": Player.objects.filter(curr_team__team_name='Penguins')}
+	elif number == 19:
+		context = {
+			"players": Player.objects.filter(curr_team__league__name='International Collegiate Baseball Conference')
+			}
+	elif number == 20:
+		context = {
+			"players": Player.objects.filter(last_name='Lopez').filter(curr_team__league__name='American Conference of Amateur Football')
+			}
+	elif number == 21:
+		context = {
+			"players": Player.objects.filter(curr_team__league__sport='Football')
+			}
+	elif number == 22:
+		context = {
+			"teams": Team.objects.filter(curr_players__first_name='Sophia')
+			}
+	elif number == 23:
+		context = {
+			"leagues": League.objects.filter(teams__curr_players__first_name='Sophia')
+			}
+	elif number == 24:
+		context = {
+			"players": Player.objects.filter(last_name='Flores').exclude(curr_team__team_name='Roughriders')
+			}
+	elif number == 25:
+		context = {
+			"teams": Team.objects.filter(all_players__first_name='Samuel').filter(all_players__last_name='Evans')
+			}
+	elif number == 26:
+		context = {
+			"players": Player.objects.filter(all_teams__team_name='Tiger-Cats')
+			}
+	elif number == 27:
+		context = {
+			"players": Player.objects.filter(all_teams__team_name='Vikings').exclude(curr_team__team_name='Vikings')
+			}
+	elif number == 28:
+		context = {
+			"teams": Team.objects.filter(all_players__first_name='Jacob').filter(all_players__last_name='Gray').exclude(team_name='Colts')
+			}
+	elif number == 29:
+		context = {
+			"players": Player.objects.filter(first_name='Joshua').filter(all_teams__league__name='Atlantic Federation of Amateur Baseball Players')
+			}
+	elif number == 30:
+		num_players =
+		context = {
+			"teams": Team.objects.annotate(num_players=Count('all_players')).exclude(num_players<12)
+			# Can't get this to work. Will fix later.
+			}
+	elif number == 31:
+		context = {
+			"players": Player.objects.annotate(num_teams=Count('all_teams')).order_by('num_teams')
+			}
 	return render(request, "leagues/index.html", context)
